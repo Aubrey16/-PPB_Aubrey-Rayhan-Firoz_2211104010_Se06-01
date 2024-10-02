4213d76d158c8705e32386ab114b4b46bd9aa12c
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2024-10-02 20:03:39 +0700

    Unguided Praktikum

---
+++ 03_Pengenalan_Dart/Unguided/tugas01.dart
@@ -0,0 +1,29 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+
+import 'dart:io';
+
+void main() {
+
+  print("Masukkan nilai: ");
+  
+
+  int nilai = int.parse(stdin.readLineSync()!);
+  
+  if(nilai > 70 ){
+    print("nilai A");
+  }else if(nilai > 40 && nilai <= 70 ){
+    print("nilai B");
+  }else if(nilai > 0 && nilai <= 40){
+    print("nilai C");
+  }else{
+    print("Kosong");
+  }
+  
+
+}
+
+// TODO: Export any libraries intended for clients of this package.
