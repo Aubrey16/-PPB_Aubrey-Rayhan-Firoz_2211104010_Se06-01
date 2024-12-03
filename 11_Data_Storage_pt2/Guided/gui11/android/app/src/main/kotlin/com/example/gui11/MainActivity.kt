24f7f8b65e8c5367ee547f803f81b8087ca857d6
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2024-12-03 09:10:25 +0700

    gui12

---
+++ 11_Data_Storage_pt2/Guided/gui11/android/app/src/main/kotlin/com/example/gui11/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gui11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 12_Maps/gui12/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MaterialApp(
+      home: MapsScreen(),
+    ));
+
+class MapsScreen extends StatefulWidget {
+  @override
+  _MapsScreenState createState() => _MapsScreenState();
+}
+
+class _MapsScreenState extends State<MapsScreen> {
+  static final LatLng _kMapCenter = LatLng(-17.535000, -149.569595);
+  static final CameraPosition _kInitialPosition = CameraPosition(
+    target: _kMapCenter
+    zoom: 11.0,
+  );
+
+  GoogleMapController? _mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Demo'),
+        centerTitle: true,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: _kInitialPosition,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+        mapType: MapType.normal, 
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _mapController?.dispose();
+    super.dispose();
+  }
+}
